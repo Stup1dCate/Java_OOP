987dee84c141821383696566a049bff42743ca3a
Tung Vu <155795286+Stup1dCate@users.noreply.github.com>
2024-10-24 14:48:15 +0700

    Update HangSX.java

--- qlxemay/HangSX.java
+++ qlxemay/HangSX.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package qlxemay;
-
+xxx
 import java.util.Scanner;
 
 public class HangSX implements INhap, IXuat {
