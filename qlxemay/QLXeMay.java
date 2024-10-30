520dfde0b2d82f10de54776a73fcb000fd538fc3
Tung Vu <155795286+Stup1dCate@users.noreply.github.com>
2024-10-30 10:27:16 +0700

    Update QLXeMay.java

--- qlxemay/QLXeMay.java
+++ qlxemay/QLXeMay.java
@@ -1,54 +1,26 @@
-/*
-  Demo các chức năng (cần có đầy đủ các chức năng xem, thêm, sửa, xóa, tìm kiếm)
-Trả lời câu hỏi về đồ án của mình. Gõ lại code theo yêu cầu của GV
-    Thang điểm:
-  (1đ) Có ít nhất 3 lớp đối tượng chính --> lớp đối tượng là các loại xe máy (lớp cha: XeMay)
-  (1đ) Các lớp phải có hàm thiết lập (constructor) -->
-  (1đ) Có kế thừa hợp lý --> các kiểu liệu (giá, hãng, năm, ... )
-  (1đ) Cài đặt và sử dụng đa hình
-  (2đ) Lớp mảng các đối tượng (danh sách)
-  (1đ) Đọc và ghi dữ liệu lên file (text) -->
-  (1đ) Có thuộc tính static, phương thức static -->
-  (1đ) Có lớp trừu tượng, hàm trừu tượng (abstract)
-  (1đ) Có interface
-Trừ điểm nếu không đủ chức năng, bị lỗi
-
-yêu cầu trong terminal: 
-1. Hiện danh sách các xe (mặc định hiện ra giống với input)
-2. Sắp xếp danh sách theo giá tiền
-3. Xem số lượng tồn kho 
-4. Xem số lượng đã bán
-5. Xem giá nhập
-                                                    CHƯƠNG TRÌNH: QUẢN LÝ CỬA HÀNG BÁN XE MÁY
-*/
-
 package qlxemay;
 import java.util.Scanner;
 
 class QLXeMay {
 	private DSXeMay dssp;
-    Scanner scanner = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
     String fileName = "input_XeMay.txt";
-	public QLXeMay()
-	{
-		dssp = new DSXeMay();
-	}	
     public void menu() {
-        int choice;
+        int select;
         do {
-        	System.out.println("1. Thêm Xe");
-            System.out.println("2. Sửa thông tin Xe");
-            System.out.println("3. Xóa Xe");
-            System.out.println("4. Tìm kiếm Xe");
-            System.out.println("5. Xem thông tin trong danh sách Xe");
-            System.out.println("6. Tải danh sách Sản phẩm từ file");
-            System.out.println("7. Xuất danh sách Sản phẩm ra file");
-            System.out.println("8. Quay trở về giao diện chính");
-            System.out.print("Chọn: ");
-            choice = scanner.nextInt();
-            scanner.nextLine(); // Đọc dòng trống sau khi đọc số
-
-            switch (choice) {
+        	System.out.println("1. Them xe may");
+            System.out.println("2. Sua thong tin xe may");
+            System.out.println("3. Xoa mot xe may");
+            System.out.println("4. Tim kiem xe may (theo masp)");
+            System.out.println("5. Xem thong tin chi tiet danh sach xe may");
+            System.out.println("6. Tai danh sach xe may tufile");
+            System.out.println("7. Xuat danh sach xe may ra file");            
+            System.out.println("8. Quay tro ve giao dien Menu chinh");
+            System.out.print("Chon: ");
+            
+            select = s.nextInt();
+            s.nextLine(); 
+            switch (select) {
             	case 1:
 	                dssp.them();
 	                break;
@@ -71,10 +43,13 @@ class QLXeMay {
                     dssp.xuatDanhSachRaFile(fileName);
                     break;
                 case 8:
+                    System.out.println("Luu thay doi va quay tro ve Menu chinh.");
                     break;
                 default:
-                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    System.out.println("Lua chon khong hop le. Vui long nhap lai.");
             }
-        } while (choice != 8);
+        } 
+        while (select != 8);
     }
 }
+
