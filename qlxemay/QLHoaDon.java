d8f93e207b30e3123a7195f894b04808cff6dce9
Tung Vu <155795286+Stup1dCate@users.noreply.github.com>
2024-10-30 10:24:06 +0700

    Update QLHoaDon.java

--- qlxemay/QLHoaDon.java
+++ qlxemay/QLHoaDon.java
@@ -4,35 +4,38 @@ import java.util.Scanner;
 
 class QLHoaDon {
 	private DSHoaDon dshd;
-    Scanner scanner = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
     String fileName = "input_HoaDon.txt";
-    String fileNameKhachHang = "KhachHang.txt";
-    String fileNameSanPham = "SanPham.txt";
+    String fileName_KhachHang = "input_KhachHang.txt";
+    String fileName_XeMay = "intput_XeMay.txt";
+    String fileName_HangSX = "input_HangSX.txt";
     DSKhachHang dsKhachHang;
     DSXeMay dsXeMay;
+    DSHangSX dsHangSX;
+
     public QLHoaDon() {
         dsKhachHang = new DSKhachHang();
-        dsKhachHang.taiDanhSachTuFile(fileNameKhachHang); // Tải danh sách khách hàng từ file
+        dsKhachHang.taiDanhSachTuFile(fileName_KhachHang); 
         dsXeMay = new DSXeMay();
         dshd = new DSHoaDon();
     }
 
     public void menu() {
-        int choice;
+        int select;
         do {
-            System.out.println("1. Thêm Hóa đơn");
-            System.out.println("2. Sửa thông tin Hóa đơn");
-            System.out.println("3. Xóa Hóa đơn");
-            System.out.println("4. Tìm kiếm Hóa đơn");
-            System.out.println("5. Xem thông tin trong danh sách Hóa đơn");
-            System.out.println("6. Tải danh sách Hóa đơn từ file");
-            System.out.println("7. Xuất danh sách Hóa đơn ra file");
-            System.out.println("8. Quay trở về giao diện chính");
-            System.out.print("Chọn: ");
-            choice = scanner.nextInt();
-            scanner.nextLine(); // Đọc dòng trống sau khi đọc số
-
-            switch (choice) {
+            System.out.println("1. Them Hoa Don");
+            System.out.println("2. Sua thong tin hoa don");
+            System.out.println("3. Xoa hoa don");
+            System.out.println("4. Tim kiem hoa don");
+            System.out.println("5. Xem thong tin chi tiet hoa don");
+            System.out.println("6. Tai danh sach hoa don tu file");
+            System.out.println("7. Xuat danh sach hoa don ra file");
+            System.out.println("8. Quay tro ve giao dien Menu chinh");
+            System.out.print("Chon: ");
+            
+            select = s.nextInt();
+            s.nextLine();
+            switch (select) {
             	case 1:
 	                dshd.them();
 	                break;
@@ -49,16 +52,18 @@ class QLHoaDon {
                 	dshd.xem();
                     break;
                 case 6:
-                    dshd.taiDanhSachTuFile(fileName, dsKhachHang, dsSanPham);
+                    dshd.taiDanhSachTuFile(fileName, dsKhachHang, dsXeMay);
                     break;
                 case 7:            
                     dshd.xuatDanhSachRaFile(fileName);
                     break;
                 case 8:
+                    System.out.println("Luu thay doi va quay tro ve Menu chinh.");
                     break;
                 default:
-                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    System.out.println("Lua chon khong hop le. Vui long nhap lai.");
             }
-        } while (choice != 8);
+        } 
+        while (select != 8);
     }
 }
