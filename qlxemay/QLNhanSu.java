8a0fc8059e2dcf516381b45266b83a438168447c
vovumewmew <vovu01673@gmail.com>
2024-10-30 07:54:17 +0700

    Update and rename QLNhanVien.java to QLNhanSu.java

--- qlxemay/QLNhanSu.java
+++ qlxemay/QLNhanSu.java
@@ -2,23 +2,23 @@ package qlxemay;
 
 import java.util.Scanner;
 
-class QLNhanVien {
-	private DSNhanSu dsnv;
+class QLNhanSu {
+	private DSNhanSu dsns;
     Scanner scanner = new Scanner(System.in);
-    String fileName1 = "input_NhanVien.txt";
-    String fileName2 = "output_NhanVien.txt";
+    String fileName1 = "input_NhanSu.txt";
+    String fileName2 = "output_NhanSu.txt";
     public QLNhanVien() {
-        dsnv = new DSNhanSu();
+        dsns = new DSNhanSu();
     }
 
     public void menu() {
         int choice;
         do {
-            System.out.println("1. Thêm Nhân viên");
-            System.out.println("2. Sửa thông tin Nhân viên");
-            System.out.println("3. Xóa Nhân viên");
-            System.out.println("4. Tìm kiếm Nhân viên");
-            System.out.println("5. Xem thông tin trong danh sách Nhân viên");
+            System.out.println("1. Thêm Nhân sự");
+            System.out.println("2. Sửa thông tin Nhân sự");
+            System.out.println("3. Xóa Nhân sự");
+            System.out.println("4. Tìm kiếm Nhân sự");
+            System.out.println("5. Xem thông tin trong danh sách Nhân sự");
             System.out.println("6. Tải danh sách Nhân viên từ file");
             System.out.println("7. Xuất danh sách Nhân viên ra file");
             System.out.println("8. Quay trở về giao diện chính");
@@ -28,19 +28,19 @@ class QLNhanVien {
 
             switch (choice) {
             	case 1:
-	                dsnv.them();
+	                dsns.them();
 	                break;
                 case 2:
-                	dsnv.sua();
+                	dsns.sua();
                     break;
                 case 3:
-                	dsnv.xoa();
+                	dsns.xoa();
                     break;
                 case 4:
-                	dsnv.timKiem();
+                	dsns.timKiem();
                     break;
                 case 5:
-                	dsnv.xem();
+                	dsns.xem();
                     break;
                 case 6:
                     dsnv.taiDanhSachTuFile(fileName1);
