75a9b73730e0c35999dfbe37ffe987eb953c2acc
akyy0707 <kyy07072005@gmail.com>
2024-10-30 10:08:18 +0700

    Update DSNhanSu.java

--- qlxemay/DSNhanSu.java
+++ qlxemay/DSNhanSu.java
@@ -48,10 +48,22 @@ class DSNhanSu {
 
 	}
 	public void Sua() {
+		System.out.println("Nhap ten nhan vien can sua: ");
+			String tennvcansua=sc.nextLine();
+			for(NhanSu ns: dsns){
+				if (ns.getTen().equals(tennvcansua)){
+					ns.Xuat();
+					
+				}
+				else{
+					System.out.println("Khong co ten nhan vien can sua");
+					return;
+				}
+			}
 		  System.out.println("Nhap ma nhan vien can sua: ");
-	        String manvcanxoa = sc.nextLine();
+	        int manvcanxoa = sc.nextInt();
 	        for(NhanSu ns : dsns) {
-	        	if (ns.getManv().equals(manvcanxoa)) {
+	        	if (ns.getManv()==manvcanxoa) {
 	                // Gọi phương thức sua() của đối tượng tài liệu tương ứng
 	                ns.Sua();
 	                System.out.println("Da sua thong tin cua nhan vien!");
@@ -62,10 +74,23 @@ class DSNhanSu {
 	}
 
 	public void Xoa() {
+		System.out.println("Nhap ten nhan vien can xoa: ");
+		String tennvcanxoa=sc.nextLine();
+			for(NhanSu ns: dsns){
+				if (ns.getTen().equals(tennvcanxoa)){
+					ns.Xuat();
+					
+				}
+				else{
+					System.out.println("Khong co ten nhan vien can xoa");
+					return;
+				}
+			}
+		  
 		System.out.println("Nhap ma nhan vien can xoa: ");
-        String manvcanxoa = sc.nextLine();
+        int manvcanxoa = sc.nextInt();
         for(NhanSu ns : dsns) {
-        	if (ns.getManv().equals(manvcanxoa)) {
+        	if (ns.getManv()==manvcanxoa) {
                 // Gọi phương thức xoa() của đối tượng tài liệu tương ứng
                 dsns.remove(ns);
                 System.out.println("Da xoa nhan vien!");
@@ -76,39 +101,49 @@ class DSNhanSu {
 	}
 	public void TimKiem() {
 		int luachon;
-		System.out.println("Ban muon tim kiem theo 1.Ma nhan vien hay 2.Ten");
-		System.out.println("Nhap lua chon cua ban: ");
-		luachon=sc.nextInt();
+		System.out.println("Bạn muốn tìm kiếm theo 1. Mã nhân viên hay 2. Tên");
+		System.out.print("Nhập lựa chọn của bạn: ");
+		luachon = sc.nextInt();
+		sc.nextLine(); // Đọc bỏ dòng mới còn sót sau khi nhập số
+	
 		switch (luachon) {
 			case 1:
-			System.out.println("Nhap ma nhan vien can tim kiem: ");
-			sc.nextLine();
-			String manv = sc.nextLine();
-			for(NhanSu ns : dsns) {
-				if (ns.getManv().equals(manv)) {
-					ns.Xuat();
+				System.out.print("Nhập mã nhân viên cần tìm kiếm: ");
+				int manv = sc.nextInt();
+				boolean foundById = false;
+				for (NhanSu ns : dsns) {
+					if (ns.getManv() == manv) {
+						ns.Xuat();
+						foundById = true;
+						break; // Ngừng tìm kiếm sau khi tìm thấy
+					}
 				}
-					System.out.println("Khong tim thay nhan vien");
-			}		
+				if (!foundById) {
+					System.out.println("Không tìm thấy nhân viên với mã này.");
+				}
 				break;
+	
 			case 2:
-			System.out.println("Nhap ten nhan vien can tim kiem: ");
-		String find = sc.nextLine();
-		boolean found = true;
-		for(NhanSu ns : dsns) {
-			if (ns.getTen().contains(find)) {
-				ns.Xuat();
-			}
-			if (!found) {
-				System.out.println("Khong co nhan vien can tim kiem !");
-			}
-		}
+				System.out.print("Nhập tên nhân viên cần tìm kiếm: ");
+				String find = sc.nextLine();
+				boolean foundByName = false;
+				for (NhanSu ns : dsns) {
+					if (ns.getTen().contains(find)) {
+						ns.Xuat();
+						foundByName = true;
+					}
+				}
+				if (!foundByName) {
+					System.out.println("Không có nhân viên cần tìm kiếm!");
+				}
 				break;
+	
 			default:
+				System.out.println("Lựa chọn không hợp lệ.");
 				break;
 		}
-		
 	}
+	
 	public void taiDanhSachTuFile(String fileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -142,8 +177,8 @@ class DSNhanSu {
   
     private NhanSu parseLineToNhanSu(String line) {
         String[] parts = line.split(";"); // Giả sử dữ liệu được phân tách bằng dấu chấm phẩy
-        if (parts.length == 6) {
-            String manv = (parts[0]);
+        if (parts.length == 7) {
+            int manv = Integer.parseInt(parts[0]);
             String ten = (parts[1]);
             int age= Integer.parseInt(parts[2]);
             String sdt = parts[3];
@@ -173,6 +208,6 @@ class DSNhanSu {
 			Quanly ql =(Quanly) ns;
 			return ns.getManv() + ";" + ns.getTen() + ";" + ns.getAge() + ";" + ns.getSDT() + ";" + ns.getChucVu() + ";" + ns.getSalary() + ";" + ql.getBonus() ;
 		}
-    }
 	return "";
+}
 }
